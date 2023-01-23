0bdd868960e19889fd84a9e3c172d627ae44de2a
Kaustub Gawade <kaustubgawade@gmail.com>
2023-01-23 13:30:18 +0530

    Spring Boot Project Added

---
+++ ecommerce-springboot-backend-app/src/main/java/com/training/ecommercespringbootbackendapp/EcommerceSpringbootBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.training.ecommercespringbootbackendapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringbootBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringbootBackendAppApplication.class, args);
+	}
+
+}
